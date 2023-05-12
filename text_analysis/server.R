2c74899895c25443328e04326f822bb16796ff59
Skywing Wang <sgtwan17@liverpool.ac.uk>
2023-05-12 16:41:17 +0100

    final code

--- text_analysis/server.R
+++ text_analysis/server.R
@@ -11,12 +11,9 @@ library(tidytext)
 library(dplyr)
 library(tidyr)
 library(qdap)
-library(sylcount)
-library(spacyr)
 library(hunspell)
 library(textstem)
 library(tokenizers)
-library(languageR)
 library(zipfR)
 library(koRpus)
 library(quanteda)
@@ -26,9 +23,23 @@ library(reshape2)
 library(igraph)
 library(stringr)
 library(word.lists)
+library(udpipe)
+library(flextable)
+if (!requireNamespace("LanguageToolR", quietly = TRUE)) {
+  install.packages("LanguageToolR")
+}
+library(LanguageToolR)
+if (!requireNamespace("DT", quietly = TRUE)) {
+  install.packages("DT")
+}
+library(DT)
+
+
+klippy::klippy()
+
 
 flesch_kincaid_data <- data.frame(
-  Score = c("100.00–90.00", "90.0–80.0", "80.0–70.0", "70.0–60.0", "60.0–50.0", "50.0–30.0", "30.0–10.0", "10.0–0.0"),
+  Score = c("100.00–90.00", "90.0–80.0", "80.0–70.0", "70.0–60.0", "60.0–50.0", "50.0–30.0", "30.0–10.0", "Less than 10.0"),
   `School level (US)` = c("5th grade", "6th grade", "7th grade", "8th & 9th grade", "10th to 12th grade", "College", "College graduate", "Professional"),
   Notes = c("Very easy to read. Easily understood by an average 11-year-old student.",
             "Easy to read. Conversational English for consumers.",
@@ -101,10 +112,26 @@ function(input, output) {
       
       lex_com_1 <- data.words.count %>% filter(word.frequency < 0.01) %>%
         group_by(doc) %>%
-        summarize(N_low_freq_words = n(), Nw = max(count.y), low_freq_ratio = n() / max(count.y)) %>%
+        summarize(N_low_freq_words = n(), Nw = max(count.y), LFW_ratio = n() / max(count.y)) %>%
         ungroup()
       
-      lex_com_1
+      familiar.words <- as.data.frame(data_char_wordlists$dalechall)
+      familiar.words$type <- "familiar"
+      colnames(familiar.words) <- c("word", "type")
+      
+      data.words.2 <- left_join(words, list_academic, by = c("word_lemma" = "lemma"))
+      
+      data.words.2 <- left_join(data.words.2, familiar.words, by = c("word_lemma" = "word"))
+      
+      lex_com_2 <- data.words.2 %>%
+        group_by(doc) %>%
+        summarize(Nw_cplx = sum(on_list == "academic" & is.na(type), na.rm = TRUE),
+                  Nw = n()) %>%
+        mutate(AWL_ratio = Nw_cplx / Nw) %>%
+        ungroup()
+      
+      final_result <- merge(lex_com_1 %>% select(doc, LFW_ratio), lex_com_2 %>% select(doc, AWL_ratio), by = "doc")
+      final_result
     } else {
       "Please enter the text on the left and click the submit button to calculate lexical complexity."
     }
@@ -122,57 +149,67 @@ function(input, output) {
     }
   })
   
-  output$growth_output <- renderPrint({
-    if (userText1() != "") {
-      # Remove all special characters using regex
-      cleaned_text <- userText1() %>%
-        qdap::bracketX() %>%
-        qdap::replace_contraction() %>%
-        gsub("[^[:alpha:][:space:]]", " ", .)
-      
-      # Tokenize
-      tokenized_text <- tibble(text = cleaned_text) %>%
-        unnest_tokens(text, output = word_token, token = "words", strip_punct = F) %>%
-        anti_join(stop_words, by = c("word_token" = "word")) %>%
-        pull(word_token)
-      
-      # Calculate growth
-      growth_result <- growth.fnc(text = tokenized_text, size = 100, nchunks = 1)
-      growth_result
-    } else {
-      "Please enter the text on the left and click the submit button to calculate growth."
-    }
-  })
+  # output$grammar_check_output <- renderTable({
+  #   if (userText1() != "") {
+  #     text_cleaned <- qdap::replace_contraction(userText1()) %>%
+  #       qdap::replace_abbreviation()
+  #     
+  #     spell_grammar_check_secondary <- LanguageToolR::languagetool(text_cleaned)
+  #     
+  #     if (nrow(spell_grammar_check_secondary) == 0) {
+  #       empty_df <- data.frame(sentence = "Congratulations, no grammatical errors have been detected!",
+  #                              short_message = "")
+  #       datatable(empty_df, options = list(pageLength = 5), rownames = FALSE)
+  #     } else {
+  #       datatable(select(spell_grammar_check_secondary, sentence, short_message),
+  #                 options = list(pageLength = 5), rownames = FALSE)
+  #     }
+  #   } else {
+  #     "Please enter the text on the left and click the submit button to get result."
+  #   }
+  # })
   
   output$syntactic_complexity_output <- renderPrint({
     if (userText1() != "") {
       text_cleaned <- qdap::replace_contraction(userText1()) %>%
         qdap::replace_abbreviation()
       
-      spacy_initialize()
-      doc1 <- spacy_parse(text_cleaned)
-      spacy_finalize()
+      # Download the language model
+      m_eng <- udpipe::udpipe_download_model(language = "english-ewt")
+      # Load the language model using the downloaded model file path
+      m_eng <- udpipe_load_model(file = m_eng$file_model)
       
-      doc1$syn_complexity_check <- (doc1$pos == "SCONJ") |
-        (doc1$pos %in% c("DET", "PRON", "ADV") & (str_detect(doc1$lemma, "^wh") | str_detect(doc1$lemma, "that"))) |
-        (doc1$pos %in% c("ADV") & (doc1$lemma %in% c("accordingly", "also", "anyway", "besides", "certainly", "consequently", "finally", "further", "furthermore", "hence", "however", "incindentally", "indeed", "instead", "likewise", "meanwhile", "moreover", "namely", "nevertheless", "next", "nonetheless", "now", "otherwise", "similarly", "still", "then", "thereafter", "therefore", "thus", "undoubtedly", "simultaneously", "firstly", "secondly", "thirdly", "lately", "yet", "again", "regardless", "rather", "comparatively")))
+      # tokenize, tag, dependency parsing
+      text_anndf <- udpipe::udpipe_annotate(m_eng, x = as.character(text_cleaned)) %>%
+        as.data.frame() %>%
+        dplyr::select(-sentence)
+      # view tagged_text
+      # head(text_anndf, 50)
+      tagged_text <- paste(text_anndf$token, "/", text_anndf$xpos, collapse = " ", sep = "")
       
-      syn_com <- doc1 %>%
+      text_anndf$syn_complexity_check <- text_anndf$upos =="SCONJ"| text_anndf$xpos %in% c("WDT", "WP", "WP$", "WRB")
+      # SCONJ: subordinating conjunction
+      # "WDT": Wh-determiner
+      # "WP": Wh-pronoun
+      # "WP$": Possessive wh-pronoun
+      # "WRB": Wh-adverb
+      check <- text_anndf %>% select(doc_id,sentence_id,token,lemma,upos,xpos,syn_complexity_check)
+      
+      syn_com <- text_anndf %>%
         group_by(doc_id, sentence_id) %>%
-        summarize(Nvb = sum(pos == "VERB"),
-                  Nstcplx = sum(syn_complexity_check == TRUE),
+        summarize(Nvb = sum(xpos=="VB"),
+                  Nstcplx = sum(syn_complexity_check==TRUE),
                   Nst = n_distinct(sentence_id),
                   Nw = n()) %>%
         group_by(doc_id) %>%
-        summarize(Nvb = sum(Nvb),
-                  Nstcplx = sum(Nstcplx > 0),
-                  Nst = sum(Nst),
-                  Nw = sum(Nw)) %>%
-        mutate(F_C = (Nvb / Nst) * (Nw / Nst),
-               Complex_sntc = Nstcplx / Nst) %>%
+        summarize(Nstcplx = sum(Nstcplx>0),
+                  Nst = sum(Nst)) %>%
+        mutate(Complex_sntc_ratio = Nstcplx / Nst) %>%
         ungroup
       
-      syn_com
+      syn_com %>%
+        select(Complex_sntc_ratio)
+      
     } else {
       "Please enter the text on the left and click the submit button to calculate syntactic complexity."
     }
@@ -182,11 +219,11 @@ function(input, output) {
   output$similarity_output <- renderPrint({
     if (userText1() != "" && userText2() != "") {
       
-      corpus <- corpus(c(userText1(),userText2()))
-      dfm <- dfm(corpus)
+      dfm <- quanteda::tokens(c(userText1(),userText2())) %>%
+        dfm()
       
       # ejaccard similarity
-      ejaccard_sim <- textstat_simil(dfm, method = "ejaccard")
+      ejaccard_sim <- textstat_simil(dfm, method = "edice")
       ejaccard_sim
     } else {
       "Please enter texts in both input fields and click the submit button to calculate the similarity comparison."
--- text_analysis/ui.R
+++ text_analysis/ui.R
@@ -17,17 +17,32 @@ fluidPage(
     mainPanel(
       tabsetPanel(id = "tabs",
                   tabPanel("Readability", value = "readability", verbatimTextOutput("readability_output"),
-                           helpText(HTML("The Flesch–Kincaid readability tests are readability tests designed to indicate how difficult a passage in English is to understand.<br>
-A high score on the Flesch–Kincaid test means that the text is easier to understand.")),
+                           helpText(HTML("The <strong> Flesch readability tests </strong> are readability tests designed to indicate how difficult a passage in English is to understand.<br>
+A high score on the Flesch test means that the text is easier to understand.")),
                            tableOutput("flesch_kincaid_table")),
                   tabPanel("Lexical Diversity", value = "lexical_diversity", verbatimTextOutput("lexical_diversity_output"),
-                           helpText(HTML("The Carroll's Corrected Type-Token Ratio (CTTR) is a measure of lexical diversity in a text.<br>
-         It is calculated by dividing the number of unique words (types) by the square root of twice the number of total words (tokens) in a text.<br>
+                           helpText(HTML("The <strong> Carroll's Corrected Type-Token Ratio (CTTR) </strong> is a measure of lexical diversity in a text.<br>
          A higher CTTR value indicates greater lexical diversity, which means that the text uses a wider variety of words."))),
-                  tabPanel("Lexical Complexity", value = "lexical_complexity", verbatimTextOutput("lexical_complexity_output")),
-                  tabPanel("Syntactic Complexity", value = "syntactic_complexity", verbatimTextOutput("syntactic_complexity_output")),
-                  tabPanel("Growth", value = "growth", verbatimTextOutput("growth_output")),
-                  tabPanel("Text Similarity", value = "similarity", verbatimTextOutput("similarity_output"))
+                  tabPanel("Lexical Complexity", value = "lexical_complexity", verbatimTextOutput("lexical_complexity_output"),
+                           helpText(HTML("These 2 methods are used in this project to measure the lexical complexity. <br><br>
+                 <strong>Academic word list (AWL) ratio</strong> <br><br>
+                 The AWL ratio reflects the percentage of words in a text included in the academic word list. <br>
+                 A higher AWL ratio indicates a more advanced academic writing level in certain texts.<br><br>
+                 <strong>Low-frequency words (LFW) ratio</strong> <br><br>
+                 A critical barrier to understanding sentences and text is the low-frequency words that make up a sentence. <br>
+                 A higher LFW ratio indicates a more complex vocabulary, suggesting that the text contains a higher proportion of relatively uncommon words."))),
+                  tabPanel("Syntactic Complexity", value = "syntactic_complexity", verbatimTextOutput("syntactic_complexity_output"),
+                           helpText(HTML("<em> The function will take some time to run, so please be patient. </em><br><br>
+                                         This function is used to determine what percentage of complex sentences are in the text.<br>
+                                         The higher the ratio, the more complex the text."))),
+                  # tabPanel("Grammar Check", value = "grammar",
+                  #          tableOutput("grammar_check_output"),
+                  #          helpText(HTML("<em>The function will take some time to run, so please be patient. </em><br><br>
+                  #          Our spell and grammar check will automatically detects and corrects typographical, grammatical, and stylistic errors in text data.<br>
+                  #      sentence: in which sentence there is a problem. <br>
+                  #      short_message: what potential problems exist that need to be checked."))),
+                  tabPanel("Text Similarity", value = "similarity", verbatimTextOutput("similarity_output"),
+                           helpText("This function compares the similarity of two functions, please enter the text to be compared."))
       )
     )
   )
